eee3142d63ea49bb262c5f11b08251c891284aa3
Gus Cost <guscost@gmail.com>
2017-04-20 17:22:11 -0400

    code: level starter module

---
+++ session2/scripts/levels.js
@@ -0,0 +1,18 @@
+// Game levels module
+var levels = (function () {
+
+  // Loader function
+  function loadLevel() {
+    state.level = {};
+    state.level.platforms = [];
+
+    // Add platforms
+    // ...
+
+  }
+
+  // Public API
+  return {
+    loadLevel: loadLevel
+  };
+})();
